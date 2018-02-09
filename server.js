8f4e32fd01a0b3ba3e77d753c6059bdfbb16332a
camposss <ccamposs23@gmail.com>
2018-02-09 13:09:38 -0800

    connected mongo database

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express= require('express');
 const PORT = process.env.port || 9000;
+const mongoose= require('mongoose');
+const keys= require('./config/keys');
 const app = express();
+
+mongoose.connect(keys.db_connect).then(()=>{
+    console.log('connected to MongoDB ');
+}).catch(err=>{
+    console.log('there was an error message ', err.message);
+});
 
 app.get('/', (req, res)=> {
     res.send('<h1>App is working</h1>');
